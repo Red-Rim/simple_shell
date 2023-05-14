be43bec133c8206b8f3bd4d2de9454869a43a584
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-05-14 03:31:38 -0700

    change

--- functions1.c
+++ functions1.c
@@ -8,15 +8,18 @@
 
 char *gettoks(char *str)
 {
+	int i;
 	const char *deliminer[] = " ";
+	char **toks = malloc((MAX + 1) * sizeof(char *));
 	char *token = strtok(str, deliminer);
 	while (token != NULL)
 	{
-		char *toks = (char*) malloc(_strlen(token) + 1);
-		strcpy(toks, token);
+		toks[i] = (char*) malloc(_strlen(token) + 1);
+		strcpy(toks[i], token);
 		token = strtok(NULL, deliminer);
-		free(toks);
+		i++;
 	}
+	toks[i] = NULL;
 
 	return(toks);
 }
