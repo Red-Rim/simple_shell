9c1f3bb8e9990404f67f619318a5ebdf6b7e9402
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-05-24 08:46:14 -0700

    task

--- simple_shell.c
+++ simple_shell.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
 	char *buffer = NULL;
 	char **cmd = NULL;
 	int exc = 0;
+	int b;
 
 	if (argc >= 2)
 	{	perror(argv[0]);
@@ -36,9 +37,17 @@ int main(int argc, char *argv[])
 		{	perror("allocation failed");
 			continue; }
 		if (_strncmp("exit", *cmd, 4) == 0 && *cmd != NULL)
-		{	freetoken(cmd);
+		{
+			if (cmd[1])
+			{
+				b = atoi(cmd[1]);
+					if (b <= -1)
+						b = 2;
+				exit(b);
+			}
+			freetoken(cmd);
 			free(buffer);
-			exit(EXIT_SUCCESS); }
+			exit(0); }
 		if (_strncmp("env", *cmd, 3) == 0 && *cmd != NULL)
 		{	_env();
 			continue; }
